d47e01cbe843c7073b4088049f1c9d42637a71c7
Thomas Steenbergen <thomas.steenbergen@here.com>
2020-03-26 21:11:51 +0100

    reporter-web-app: Sort licenses selectors in package finding table
    Signed-off-by: Thomas Steenbergen <thomas.steenbergen@here.com>

--- reporter-web-app/src/components/PackageFindingsTable.js
+++ reporter-web-app/src/components/PackageFindingsTable.js
@@ -84,6 +84,17 @@ class PackageFindingsTable extends React.Component {
                 filters: (
                     () => Array.from(webAppPackage.detectedLicenses)
                         .map((license) => ({ text: license, value: license }))
+                        .sort(
+                            (a, b) => {
+                                if (a.text < b.text) {
+                                    return -1;
+                                }
+                                if (a.text > b.text) {
+                                    return 1;
+                                }
+                                return 0;
+                            }
+                        )
                 )(),
                 onFilter: (value, row) => value === row.value,
                 key: 'value',
